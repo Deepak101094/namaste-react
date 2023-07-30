c859b189a638b4960dd373cc899c4d06fad1d7e3
Deepak/101094 <deepakpandey101094@gmail.com>
2023-07-30 20:59:05 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+	"div",
+	{ id: "parent" },
+	React.createElement("div", { id: "child" }, [
+		React.createElement("h1", { id: "h1" }, "hello this from h1 tag"),
+		React.createElement("h2", { id: "h2" }, "this is from h2 tag"),
+	])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
